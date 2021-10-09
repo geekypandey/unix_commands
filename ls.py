abf0cb8be786af5efe3694b5f21fa70863dd7306
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-10-09 11:21:04 +0530

    working

--- ls.py
+++ ls.py
@@ -3,6 +3,8 @@ import os
 
 def ls(dirname=None, show_hidden=False, reverse=False) -> list:
     """Lists the contents of the directory specified."""
+    if dirname is None:
+        dirname = "."
     resp = sorted(os.listdir(dirname), reverse=reverse)
 
     if not show_hidden:
