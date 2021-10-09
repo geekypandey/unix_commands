4041dd6402bcd722ebfed239cba4c07f46b45260
Anurag Pandey <anuragcarbon1998@gmail.com>
2021-10-09 11:29:54 +0530

    working

--- test_ls.py
+++ test_ls.py
@@ -80,7 +80,7 @@ class TestLsCommand(unittest.TestCase):
 
     def test_ls_command_with_home_directory_as_tilde(self) -> None:
         HOME = os.path.expanduser("~")
-        folder = self.create_temp_folders(dirname=HOME, prefix=".")
+        folder = self.create_temp_folders(dirname=HOME, prefix=".")[0]
         pass
 
     def tearDown(self) -> None:
